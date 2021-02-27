dd84c56f501839072adb04fcbf18fef8ae364f99
JILandos Sergendaliev <nail-home@mail.ru>
2021-02-27 21:03:40 +0600

    Добавлен поиск запятой

---
+++ geometry.c
@@ -0,0 +1,30 @@
+#include <ctype.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+bool finding_comma(char *UKAZATEL)
+{
+    int flag = 0;
+    while (*UKAZATEL != 10) {
+        if (*UKAZATEL == ',') {
+            flag = 1;
+            break;
+        }
+        UKAZATEL++;
+    }
+    if (flag == 0)
+        return false;
+    return true;
+}
+    int main()
+    {
+        int Q = 228;
+        double Z[339];
+        char str[Q];
+        char Circle[] = {"circle"};
+        fgets(str);
+        finding_comma(str);
+    return 0;
+}
