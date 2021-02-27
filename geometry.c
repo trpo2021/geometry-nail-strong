9c6b06d698b3931178144e17ae43166d35b911ad
JILandos Sergendaliev <nail-home@mail.ru>
2021-02-27 21:12:58 +0600

    Добавлен второй указатель для перехода на первую скобку команды, объеденил все функции поиска в одну проверяющую

--- geometry.c
+++ geometry.c
@@ -46,15 +46,43 @@ bool finding_bracket2(char *UKAZATEL)
         return false;
     return true;
 }
-    int main()
-    {
-        int Q = 228;
-        double Z[339];
-        char str[Q];
-        char Circle[] = {"circle"};
-        fgets(str);
-        finding_comma(str);
-        finding_bracket(str);
-        finding_bracket2(str);
-    return 0;
+bool correct(char* Circle, char *UKAZATEL, int Z)
+{
+    int w = 6, check = 0;
+    if (strncmp(Circle, UKAZATEL, w) == 0) {
+        check++;
+        if (finding_bracket(UKAZATEL) == true)
+            check++;
+        if (finding_comma(UKAZATEL) == true)
+            check++;
+        if (finding_bracket2(UKAZATEL) == true)
+            check++;
+    } else {
+        printf("error: Check the spelling of the command");
+    }
+    if (check == 4)
+        return true;
+    if (check != 4)
+        return false;
+    return true;
+}
+int main()
+{
+    int Q = 228;
+    double Z[339];
+    char str[Q];
+    char *UKAZATEL = str;
+    char *UKAZATEL2 = str;
+    char Circle[] = {"circle"};
+fgets(str, Q, stdin);
+if (isalpha(*UKAZATEL) != 0) {
+    while (isalpha(*UKAZATEL2) != 0)
+        UKAZATEL2++;
+}
+if (correct(Circle, str, Q) == true) {
+    UKAZATEL = UKAZATEL2;
+    printf("approved\n");
+}
+system("pause");
+return 0;
 }
