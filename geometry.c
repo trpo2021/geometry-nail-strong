45e075b60937df99928e1219342696fc0d6f30a3
JILandos Sergendaliev <nail-home@mail.ru>
2021-02-27 21:05:53 +0600

    Добавлен поиск первой и второй скобки

--- geometry.c
+++ geometry.c
@@ -18,6 +18,34 @@ bool finding_comma(char *UKAZATEL)
         return false;
     return true;
 }
+bool finding_bracket(char *UKAZATEL)
+{
+    int flag = 0;
+    while (*UKAZATEL != 10) {
+        if (*UKAZATEL == '(') {
+            flag = 1;
+            break;
+        }
+        UKAZATEL++;
+    }
+    if (flag == 0)
+        return false;
+    return true;
+}
+bool finding_bracket2(char *UKAZATEL)
+{
+    int flag = 0;
+    while (*UKAZATEL != 10) {
+        if (*UKAZATEL == ')') {
+            flag = 1;
+            break;
+        }
+        UKAZATEL++;
+    }
+    if (flag == 0)
+        return false;
+    return true;
+}
     int main()
     {
         int Q = 228;
@@ -26,5 +54,7 @@ bool finding_comma(char *UKAZATEL)
         char Circle[] = {"circle"};
         fgets(str);
         finding_comma(str);
+        finding_bracket(str);
+        finding_bracket2(str);
     return 0;
 }
