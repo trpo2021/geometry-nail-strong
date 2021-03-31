9d7b19894e02757eceee67576ff026833909f6bf
JILandos Sergendaliev <nail-home@mail.ru>
2021-03-31 14:20:13 +0600

    make lib_input.h

---
+++ lib_input.h
@@ -0,0 +1,10 @@
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#pragma once
+bool finding_comma(char *UKAZATEL)
+bool finding_bracket(char *UKAZATEL)
+bool finding_bracket2(char *UKAZATEL)
+bool correct(char* Circle, char *UKAZATEL, int Z)
