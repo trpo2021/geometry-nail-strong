6ca391b54ae7e0e12a2aed6a2120689bc334c719
nail-strong <nail-home@mail.ru>
2021-05-07 21:46:07 +0600

    added a second circle

--- src/geometry/main.c
+++ src/geometry/main.c
@@ -11,6 +11,7 @@ int main()
 {
     int Q = 228;
     double Z[339];
+    double P[159];
     char str[Q];
     char* UKAZATEL = str;
     char* UKAZATEL2 = str;
@@ -27,6 +28,22 @@ int main()
     }
     translation_universal(UKAZATEL, Z);
     calculation(Z);
+    char str2[Q];
+    char* KRUG1 = str2;
+    char* KRUG2 = str2;
+    fgets(str2, Q, stdin);
+    if (isalpha(*KRUG1) != 0) {
+        while (isalpha(*KRUG2) != 0)
+            KRUG2++;
+    }
+    if (correct(Circle, str2) == true) {
+        KRUG1 = KRUG2;
+        printf("approved\n");
+        correct(Circle, KRUG1);
+    }
+    translation_universal(KRUG1, P);
+    calculation(P);
+    circle(Z, P);
     system("pause");
     return 0;
 }
